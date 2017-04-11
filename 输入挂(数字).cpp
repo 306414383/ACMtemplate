f581d2a1bc9c458b1be34fc681b89755227a393d
lb306414383 <306414383@qq.com>
2017-04-11 21:15:07 +0800

    170411

---
+++ 输入挂(数字).cpp
@@ -0,0 +1,41 @@
+const int SIZE=500050;///读入长度
+
+char buffer[SIZE];
+int pointer = SIZE;
+
+char Advance() {
+    if (pointer == SIZE) {
+        fread(buffer, 1, SIZE, stdin);
+        pointer = 0;
+    }
+    return buffer[pointer++];
+}
+
+int Read() {
+    int answer = 0;
+    char ch = Advance();
+    while (!isdigit(ch))
+        ch = Advance();
+    while (isdigit(ch)) {
+        answer = answer * 10 + ch - '0';
+        ch = Advance();
+    }
+    return answer;
+}
+
+///带负数
+int Read() {
+    int answer = 0, sign = 1;
+    char ch = Advance();
+    while (!isdigit(ch) && ch != '-')
+        ch = Advance();
+    if (ch == '-') {
+        sign = -1;
+        ch = Advance();
+    }
+    while (isdigit(ch)) {
+        answer = answer * 10 + ch - '0';
+        ch = Advance();
+    }
+    return answer * sign;
+}
